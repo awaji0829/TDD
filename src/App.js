7d98d9be2fe0934d7349a061c4f0ac9397c4ad75
kangminji <awaji0829@gmail.com>
2022-01-17 07:48:49 +0900

    버튼 disabled test 작성

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
   return (
     <div className='App'>
       <header className='App-header'>
@@ -11,14 +12,25 @@ function App() {
           <button
             onClick={() => setCounter((num) => num - 1)}
             data-testid='minus-button'
+            disabled={disabled}
           >
             -
           </button>
           <button
             onClick={() => setCounter((num) => num + 1)}
             data-testid='plus-button'
+            disabled={disabled}
           >
             +
+          </button>
+        </div>
+        <div>
+          <button
+            data-testid='on/off-button'
+            style={{ backgroundColor: 'blue' }}
+            onClick={() => setDisabled((prev) => !prev)}
+          >
+            on/off
           </button>
         </div>
       </header>
--- src/App.test.js
+++ src/App.test.js
@@ -37,3 +37,21 @@ test('마이너스 누르면 1추가', () => {
   const counterElement = screen.getByTestId('counter');
   expect(counterElement).toHaveTextContent(-1);
 });
+test('온오프버튼 색깔 있니', () => {
+  render(<App />);
+  //sreen object 를 이용해서 원하는 엘레멘트에 접근(접근할때 id로 )
+
+  const buttonElement = screen.getByTestId('on/off-button');
+
+  // const buttonElement = screen.getByTestId('counter');
+  expect(buttonElement).toHaveStyle({ backgroundColor: 'blue' });
+});
+test('온오프버튼 클릭했을때 +/- 버튼 비활', () => {
+  render(<App />);
+
+  const onOffButtonElement = screen.getByTestId('on/off-button');
+  fireEvent.click(onOffButtonElement);
+  const plusButtonElement = screen.getByTestId('plus-button');
+
+  expect(plusButtonElement).toBeDisabled();
+});
